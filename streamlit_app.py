f49571f69f86a1e140f6b6757caeb7d4b0d97805
haydenclose <hayden.close@cefas.gov.uk>
2023-08-15 21:21:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ st.title("fudge you streamlit")
 in_csv = os.path.join(os.getcwd(), "data/Wreck Database_V2.4.csv")
 wrecks = pd.read_csv(in_csv)
 wreck_name = 'HMS REPULSE'
-lat = pd.to_numeric(wecks.loc[wrecks['wreck_ID'] == wreck_name]['Latitude'])               # Get the latitude of selected wreck
-lon = wrecks.loc[wrecks['wreck_ID'] == wreck_name['Longitude']                             # Get the longitude of selected wreck
+lat = pd.to_numeric(wecks.loc[wrecks['wreck_ID'] = wreck_name]['Latitude'])               # Get the latitude of selected wreck
+lon = wrecks.loc[wrecks['wreck_ID'] = wreck_name['Longitude']                             # Get the longitude of selected wreck
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 st.write(float(Lat),float(Lon))
