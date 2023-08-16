e31c71a22d6f9cbba5d89e64ca9adddb54c7e630
haydenclose <hayden.close@cefas.gov.uk>
2023-08-16 10:20:47 +0100

    Update ExamplePage.py

--- pages/ExamplePage.py
+++ pages/ExamplePage.py
@@ -18,25 +18,36 @@ In the meantime, below is an example of what you can do with just a few lines of
 st.title("fudge you streamlit")
 in_csv = os.path.join(os.getcwd(), "data/Wreck Database_V2.4.csv")
 wrecks = pd.read_csv(in_csv)
-wreck_name = 'HMS REPULSE'
+wreck_names = wrecks["Wreck_ID"].values.tolist()
+wreck_name = st.selectbox("Select a wreck:", wreck_names, index = 2)
 lat = pd.to_numeric(wrecks.loc[wrecks['Wreck_ID'] == wreck_name]['Latitude'])               # Get the latitude of selected wreck
 lon = pd.to_numeric(wrecks.loc[wrecks['Wreck_ID'] == wreck_name]['Longitude'])                             # Get the longitude of selected wreck
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-st.write(lat,lon)
-Point = namedtuple('Point', 'x y')
-data = []
+S1Thresh = st.slider("S1 Threshold:", -30, -15,(-30,-23.5),.1)
+S2Thresh = st.slider("S2 threshold:", 1500, 7000,(-2350,3000),25)
 
-points_per_turn = total_points / num_turns
+Sat_options = ['SENTINEL-1','SENTINEL-2', 'BOTH']
+Satellite_selection = st.radio("Select an Satellite:", Sat_options, index = 2)
 
-for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+Orbit_options = ['ASCENDING','DESCENDING','BOTH']
+Orbit_selection = st.radio("Select an orbit:", Sat_options, index = 2)
 
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+max_cloud_cover = st.slider("Max cloud %:", 0, 1000,100,1)
+Dilation = st.slider("Pixel Dilation factor:", 0, 20,3,1)
+PixelFilter = st.slider("Remove polygons smaller than:", 0, 1000,250,1)
+
+def Next_button():
+    st.write(" Next Button clicked!")
+
+if st.button("Next image"):
+    Next_button()
+
+def my_function():
+    st.write(" Previous Button clicked!")
+
+if st.button("Next image"):
+    Previous_button()
+
+
+
+
+
